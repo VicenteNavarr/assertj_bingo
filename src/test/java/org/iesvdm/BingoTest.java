391a2dcfc32322bdf53e503db7e4d1034b88ed8c
administrador <vicentenavarr@gmail.com>
2024-04-03 10:22:03 +0200

    03/04/2024

---
+++ src/test/java/org/iesvdm/BingoTest.java
@@ -0,0 +1,22 @@
+package org.iesvdm;
+
+
+import org.assertj.core.api.Assertions;
+import org.testng.Assert;
+import org.testng.asserts.Assertion;
+
+public class BingoTest {
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
